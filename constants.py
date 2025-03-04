486f6ef06839a563d321206f3bb374ff00965f07
imenryu <70045261+imenryu@users.noreply.github.com>
2025-03-04 14:49:26 +0530

    Update constants.py

--- constants.py
+++ constants.py
@@ -2,42 +2,30 @@ import os
 import json
 import random
 
-# Pokèmon
-SAFARI = set([
-    "",
-])
+# Pokémon Categories
+SAFARI = set([])
 
-NEST_BALL = set([
-    "",
-])
+NEST_BALL = set([])
 
 REPEAT_BALL = set([
- "Venusaur", "Charizard", "Blastoise", "Beedrill", "Alakazam", "Slowbro",
- "Mewtwo", "Aerodactyl", "Ampharos", "Steelix", "Scizor",
- "Sceptile", "Blaziken", "Swampert", "Gardevoir", "Sableye", "Mawile",
- "Aggron", "Medicham", "Manectric", "Sharpedo", "Camerupt", "Altaria", "Banette", "Absol", "Glalie",
- "Metagross", "Lucario", "Abomasnow", "Gallade", "Audino",
- "Blacephalon", "Buzzwole", "Cobalion", "Cosmog", "Cosmoem", "Deoxys", "Diancie", "Dialga", 
- "Eternatus", "Genesect", "Giratina", "Glastrier", "Groudon", "Ho-oh", "Hoopa", "Jirachi", 
- "Kartana", "Keldeo", "Kubfu", "Kyogre", "Kyurem", "Landorus", "Lugia", "Magearna", "Marshadow", 
- "Mewtwo", "Necrozma", "Palkia", "Pheromosa", "Rayquaza", "Regieleki", "Regigigas", "Reshiram", 
- "Shaymin", "Spectrier", "Terrakion", "Victini", "Virizion", "Xerneas", "Yveltal", "Zacian", 
- "Zamazenta", "Zekrom", "Zeraora", "Zygarde"
+    "Venusaur", "Charizard", "Blastoise", "Beedrill", "Alakazam", "Slowbro",
+    "Mewtwo", "Aerodactyl", "Ampharos", "Steelix", "Scizor",
+    "Sceptile", "Blaziken", "Swampert", "Gardevoir", "Sableye", "Mawile",
+    "Aggron", "Medicham", "Manectric", "Sharpedo", "Camerupt", "Altaria", "Banette", "Absol", "Glalie",
+    "Metagross", "Lucario", "Abomasnow", "Gallade", "Audino",
+    "Blacephalon", "Buzzwole", "Cobalion", "Cosmog", "Cosmoem", "Deoxys", "Diancie", "Dialga",
+    "Eternatus", "Genesect", "Giratina", "Glastrier", "Groudon", "Ho-oh", "Hoopa", "Jirachi",
+    "Kartana", "Keldeo", "Kubfu", "Kyogre", "Kyurem", "Landorus", "Lugia", "Magearna", "Marshadow",
+    "Mewtwo", "Necrozma", "Palkia", "Pheromosa", "Rayquaza", "Regieleki", "Regigigas", "Reshiram",
+    "Shaymin", "Spectrier", "Terrakion", "Victini", "Virizion", "Xerneas", "Yveltal", "Zacian",
+    "Zamazenta", "Zekrom", "Zeraora", "Zygarde"
 ])
 
-ULTRA_BALL = set([
- "",
-])
+ULTRA_BALL = set([])
+GREAT_BALL = set([])
+REGULAR_BALL = set([])
 
-GREAT_BALL = set([
- "",
-])
-
-REGULAR_BALL = set([
- "",
-])
-
-# owner username and bot version 
+# Owner and Bot Information
 OWNER_NAME = "Enryu"
 BOT_VERSION = "1.0"
 
@@ -48,22 +36,27 @@ HELP_COMMAND_REGEX = r'^\.help$'
 EVAL_COMMAND_REGEX = r'^\.eval (.+)'
 GUESSER_COMMAND_REGEX = r'^\.guess (on|off|stats)$'
 HUNTER_COMMAND_REGEX = r'^\.hunt (on|off|stats)$'
+LIST_COMMAND_REGEX = r'^\.list$'  # Added .list command
 
 # AFK Commands
 AFK_COMMAND_REGEX = r'^\.afk(?: |$)(.*)'  # Matches `.afk` or `.afk <message>`
 UNAFK_COMMAND_REGEX = r'^\.unafk$'  # Matches `.unafk`
 
+# Timing and Limits
 COOLDOWN = lambda: random.randint(3, 6)
 PERIODICALLY_GUESS_SECONDS = 120
 PERIODICALLY_HUNT_SECONDS = 300
 HEXA_BOT_ID = 572621020
 
+# API Credentials
 API_ID = int(os.getenv('API_ID'))
 API_HASH = os.getenv('API_HASH')
 SESSION = os.getenv('SESSION')
 
+# Chat ID
 CHAT_ID = int(os.getenv('CHAT_ID'))
 
+# Load Pokémon Data
 with open('pokemon.json', 'r') as f:
     POKEMON = json.load(f)
 
